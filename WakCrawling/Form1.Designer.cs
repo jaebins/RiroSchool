2a557e6e04a93b13a3b13f3ce44a7e625a0c0286
jaebins <jen2065@naver.com>
2023-06-27 10:20:18 +0900

    초기 커밋.

---
+++ WakCrawling/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace WakCrawling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.input_id = new System.Windows.Forms.TextBox();
+            this.input_pw = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.but_Login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(0, 190);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1140, 413);
+            this.textBox1.TabIndex = 0;
+            // 
+            // input_id
+            // 
+            this.input_id.Location = new System.Drawing.Point(466, 26);
+            this.input_id.Name = "input_id";
+            this.input_id.Size = new System.Drawing.Size(187, 21);
+            this.input_id.TabIndex = 1;
+            // 
+            // input_pw
+            // 
+            this.input_pw.Location = new System.Drawing.Point(466, 81);
+            this.input_pw.Name = "input_pw";
+            this.input_pw.PasswordChar = '*';
+            this.input_pw.Size = new System.Drawing.Size(187, 21);
+            this.input_pw.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(425, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(425, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PW : ";
+            // 
+            // but_Login
+            // 
+            this.but_Login.Location = new System.Drawing.Point(427, 132);
+            this.but_Login.Name = "but_Login";
+            this.but_Login.Size = new System.Drawing.Size(226, 35);
+            this.but_Login.TabIndex = 5;
+            this.but_Login.Text = "리로스쿨 로그인";
+            this.but_Login.UseVisualStyleBackColor = true;
+            this.but_Login.Click += new System.EventHandler(this.but_Login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 594);
+            this.Controls.Add(this.but_Login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input_pw);
+            this.Controls.Add(this.input_id);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "연초고 리로스쿨";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox input_id;
+        private System.Windows.Forms.TextBox input_pw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button but_Login;
+    }
+}
+
---
+++ WakCrawling/Form1.cs
@@ -0,0 +1,87 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WakCrawling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void but_Login_Click(object sender, EventArgs e)
+        {
+            string loginUrl = "https://yeoncho.riroschool.kr/ajax.php";
+            string postUrl = "https://yeoncho.riroschool.kr/portfolio.php?db=1502&t_doc=0";
+
+            string data = $"app=user&mode=login&userType=1&id={input_id.Text}&pw={input_pw.Text}";
+            byte[] buffer = Encoding.UTF8.GetBytes(data);
+
+            CookieContainer cookies = new CookieContainer();
+
+            try
+            {
+                WebResponse loginRes = GetHtml(loginUrl, buffer, cookies);
+                if(loginRes.ContentLength != 88)
+                {
+                    MessageBox.Show("아이디 또는 비밀번호가 일치하지 않습니다.");
+                    return;
+                }
+                WebResponse postRes = GetHtml(postUrl, null, cookies);
+
+                Stream stream = postRes.GetResponseStream();
+                StreamReader streamReader = new StreamReader(stream, Encoding.UTF8, false);
+
+                string html = streamReader.ReadToEnd();
+                HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+                htmlDoc.LoadHtml(html);
+
+                HtmlNodeCollection descNodes = htmlDoc.DocumentNode.SelectNodes("//*[@id='container']/div/div[3]/table/tr/td[3]");
+                RsAppend(descNodes);
+
+                //textBox1.AppendText(html);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+            }
+        }
+
+        WebResponse GetHtml(string url, byte[] buffer, CookieContainer cookies)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "post";
+            req.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            req.CookieContainer = cookies;
+
+            if (buffer != null)
+            {
+                req.GetRequestStream().Write(buffer, 0, buffer.Length);
+            }
+
+            WebResponse res = req.GetResponse();
+            return res;
+        }
+
+        void RsAppend(HtmlNodeCollection nodes)
+        {
+            for (int i = 1; i < nodes.Count; i++)
+            {
+                string text = nodes[i].InnerText.Replace("  ", "");
+                textBox1.AppendText(text + Environment.NewLine + Environment.NewLine);
+            }
+        }
+    }
+
+}
